5d062bc677cd43ff11829d428236d6f35f993157
Nikman800 <76964182+Nikman800@users.noreply.github.com>
2024-03-28 01:33:37 -0700

    Added Procfile for heroku, removed some comments

--- server/main.py
+++ server/main.py
@@ -1,25 +1,3 @@
-# from flask import Flask, request, jsonify
-# from flask_cors import CORS
-
-# app = Flask(__name__)
-# cors = CORS(app, origins='*')
-
-# @app.route("/api/users", methods=["GET"])
-# def users():
-#     return jsonify(
-#         {
-#             "users": [
-#                 'Alice',
-#                 'Bob',
-#                 'Charlie' 
-#             ]      
-#         }
-                 
-#     )
-
-# if __name__ == "__main__":
-#     app.run(debug=True, port=8080)
-
 from flask import Flask, request, jsonify
 import cv2
 import numpy as np
